e0acf7518c7cdd49dc7374126a8bb6e4c1400d5d
aluno <aluno@192.168.10.192>
2013-03-20 22:46:40 -0300

    Fornecedor: Classe fornecedor em declarada com construtor criado.

---
+++ Compras/src/projeto1/Fornecedor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Fornecedor {
+    int codigo;
+    String nome;
+    int cnpj;
+    String bairro;
+    String rua;
+    int num_rua;
+    String tel;
+    String email;
+    
+    Fornecedor(int codigo, String nome, int cnpj, String bairro, String rua, int num_rua, String tel, String email){
+    }
+     
+    }
+    
+}
